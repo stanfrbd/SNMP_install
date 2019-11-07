da4641f7abf63b95d56403e31b9e3dddd1c13f67
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2019-11-07 06:24:25 +0100

    Added comments

--- [Multi]SNMP_install.ps1
+++ [Multi]SNMP_install.ps1
@@ -4,16 +4,19 @@
       Install and configure SNMP on a multi servers
       REMEMBER: the server must be reachable. If not, you will get errors.
       It will be a password prompt for each remote command.
+      The public commnity will have READ-ONLY privilegies
+      Replace 10.0.0.0 with your trusted IP address
+      You can change the Community Name but this is not recommanded
 #>
 $adminLogin = "yourAdminLogin"
-$servers = "10.0.0.0", "10.0.0.0" <# enter #>
+$servers = "10.0.0.0", "10.0.0.0" <# enter others here #>
 
 foreach ($server in $servers) {
     <# Check if SNMP-Service is already installed
     $check = Get-WindowsFeature -computername $server -Credential smedrano.admin -Name SNMP-Service, Failover-Clustering 
     if (-not($check.installed)) { #>
-    Write-Host "Installation and configuration: SNMP on $server"
-    Install-WindowsFeature -computername $server -Credential $adminLogin -Name SNMP-Service, Failover-Clustering -IncludeManagementTools
+    Write-Host "Install and configure: SNMP on $server"
+    Install-WindowsFeature -computername $server -Credential $adminLogin -Name SNMP-Service -IncludeManagementTools
         
     <# create et install .reg file#>
     Invoke-Command -ComputerName $server -Credential $adminLogin { if (-not(Test-Path -Path "C:\temp")) { mkdir C:\temp } }
@@ -23,12 +26,12 @@ foreach ($server in $servers) {
         "EnableAuthenticationTraps"=dword:00000000
          
         [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers]
-        "1"="10.80.3.250"
+        "1"="10.0.0.0"
          
         [-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\public]
          
         [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\public]
-        "1"="allowed.domain/orIP"
+        "1"="10.0.0.0"
          
         [-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities]
          
